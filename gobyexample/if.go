5c28a469513391875c8001af06626c755fd507ec
jun <jsgygujun@gmail.com>
2021-03-09 08:55:34 +0800

    [ADD] For 循环

---
+++ gobyexample/if.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if num := 9; num < 0 { // 在条件语句之前可以有一个声明语句，在这里声明的变量可以在这个语句所有的条件分支中使用。
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+}
