d61cec93066b841d9b622e5d118f81e0ef28f520
jun <jsgygujun@gmail.com>
2021-03-09 08:45:33 +0800

    [UPDATE] 注释

--- gobyexample/variable.go
+++ gobyexample/variable.go
@@ -6,7 +6,7 @@ func main() {
 	var a = "initial"
 	fmt.Println(a)
 
-	var b, c int = 1, 2
+	var b, c int = 1, 2 // 一次声明多个变量
 	fmt.Println(b, c)
 
 	var d = true
@@ -15,6 +15,6 @@ func main() {
 	var e int // 声明后却没有给出对应的初始值时，变量将会初始化为 零值 。
 	fmt.Println(e)
 
-	f := "short" // 简写
+	f := "short" // 简写声明
 	fmt.Println(f)
 }
