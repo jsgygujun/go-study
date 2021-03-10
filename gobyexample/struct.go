50946280e38a0022183430619b8a2ec249c3e58a
jsgygujun <jsgygujun@gmail.com>
2021-03-10 17:27:17 +0800

    [UPDATE] 添加注释

--- gobyexample/struct.go
+++ gobyexample/struct.go
@@ -8,15 +8,15 @@ type Person struct {
 }
 
 func main() {
-	fmt.Println(Person{"Bob", 20})
-	fmt.Println(Person{name: "Alice", age: 30})
-	fmt.Println(Person{name: "Fred"})
-	fmt.Println(&Person{name: "Ann", age: 40})
+	fmt.Println(Person{"Bob", 20})              // 创建了一个新的结构体元素
+	fmt.Println(Person{name: "Alice", age: 30}) // 初始化一个结构体元素时指定字段名字
+	fmt.Println(Person{name: "Fred"})           // 省略的字段将被初始化为零值
+	fmt.Println(&Person{name: "Ann", age: 40})  // & 前缀生成一个结构体指针
 	s := Person{name: "Sean", age: 50}
 	fmt.Println(s.name)
 
 	sp := &s
 	fmt.Println(sp.age)
-	sp.age = 51
+	sp.age = 51 // 结构体是可变的
 	fmt.Println(sp.age)
 }
